aa91c67b769afb78cc1a548b40f11713471ad1b4
Tallis <talliselder54@gmail.com>
2022-10-24 16:44:23 +0100

    Made it modular

--- index.py
+++ index.py
@@ -1,16 +1,21 @@
-# First set of arrays
-firstName = []
-surname = []
-examMarks = []
-
 # Start of score module
-with open("demo files/Studentmarks.txt", "rt") as f:
-    for each_line in f:
-        tmp, score = each_line.split(' : ')
-        first, last = tmp.split(' ')
+def readFile():
 
-        examMarks.append(score)
-        firstName.append(first)
-        surname.append(last)
-f.close()
+    examMarks = []
+    firstName = []
+    surname = []
+
+    with open("demo files/Studentmarks.txt", "rt") as f:
+        for each_line in f:
+            tmp, score = each_line.split(' : ')
+            first, last = tmp.split(' ')
+
+            examMarks.append(score)
+            firstName.append(first)
+            surname.append(last)
+    f.close()
+
+    return examMarks, firstName, surname
 # End of score module
+
+examMarks, firstName, surname = readFile()
