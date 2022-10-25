f8b854558c4ed511e167436cfac3286568f6734b
Tallis <talliselder54@gmail.com>
2022-10-25 15:24:07 +0100

    Final product

--- index.py
+++ index.py
@@ -1,6 +1,4 @@
-# Start of score module
 def readFile():
-
     examMarks = []
     firstName = []
     surname = []
@@ -10,12 +8,43 @@ def readFile():
             tmp, score = each_line.split(' : ')
             first, last = tmp.split(' ')
 
-            examMarks.append(score)
+            examMarks.append(int(score[:-1]))
             firstName.append(first)
             surname.append(last)
-    f.close()
 
     return examMarks, firstName, surname
-# End of score module
+
+
+def highestMark(examMarks, firstName, surname):
+    maximum = 0
+    pos = -1
+
+    for index in range(len(examMarks)):
+
+        if examMarks[index] > maximum:
+            maximum = examMarks[index]
+            pos = index
+
+    highestFirstName = firstName[pos]
+    highestSurname = surname[pos]
+
+    return highestFirstName, highestSurname, maximum
+
+
+def sendResults(highestFirstName, highestSurname, maximum):
+    print("The top mark was", maximum, "by", highestFirstName, highestSurname)
+
+
+def newFile(firstName, surname, examMarks):
+    f = open("NewFileResults.txt", "w")
+
+    for index in range(50):
+        fileInput = firstName[index] + " " + surname[index] + "has a score of " + str(examMarks[index])
+
+        f.write(fileInput + "\n")
+
 
 examMarks, firstName, surname = readFile()
+highestFirstName, highestSurname, maximum = highestMark(examMarks, firstName, surname)
+sendResults(highestFirstName, highestSurname, maximum)
+newFile(firstName, surname, examMarks)
